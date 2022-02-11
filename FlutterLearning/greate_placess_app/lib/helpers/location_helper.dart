7895a685da05018d9451d6eff16d551d63f91a8a
kazmkorkmaz <56412106+kazmkorkmaz@users.noreply.github.com>
2022-02-11 17:21:39 +0300

    Delete FlutterLearning directory

--- FlutterLearning/greate_placess_app/lib/helpers/location_helper.dart
+++
@@ -1,9 +0,0 @@
-const MAP_BOX_API_KEY =
-    'sk.eyJ1Ijoia2F6bWtvcmttYXo1NSIsImEiOiJja3lyajBjd3EwZGNjMnlzMDZna3lxdXQyIn0.TsyDag3WBAtjn13Tb-3wlw';
-
-class LocationHelper {
-  static String generateLocationPreviewImage(
-      double latitude, double longitude) {
-    return 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-l($longitude,$latitude)/$longitude,$latitude,14.25,0,0/600x300?access_token=$MAP_BOX_API_KEY';
-  }
-}
